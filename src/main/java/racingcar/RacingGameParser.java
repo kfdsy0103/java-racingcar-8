cd916a906d320e7e331ab09424f8902761ff7ae8
pywoo <kfdsy0103@naver.com>
2025-10-26 01:18:22 +0900

    fix(RacingGameParser): NPE 문제 수정
    parseRacingCarNames로 들어오는 입력 문자열에 대한 NPE 문제를 해결하였습니다.

--- src/main/java/racingcar/RacingGameParser.java
+++ src/main/java/racingcar/RacingGameParser.java
@@ -6,6 +6,10 @@ import java.util.List;
 public class RacingGameParser {
 
     public static List<String> parseRacingCarNames(String input) {
+        if (input == null || input.isBlank()) {
+            throw new IllegalArgumentException("자동차 이름을 다시 입력해주세요.");
+        }
+
         List<String> racingCarNameList = Arrays.stream(input.split(",")).toList();
         racingCarNameList.forEach(carName -> RacingGameValidator.validateRacingCarName(carName));
         return racingCarNameList;
