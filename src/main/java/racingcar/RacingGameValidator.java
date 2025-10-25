c98efa4f54d85fe238e5a1d3e3efa681b0cd7bfb
pywoo <kfdsy0103@naver.com>
2025-10-25 18:48:47 +0900

    feat: 입력값 검증 기능 구현
    validateRacingCarName() : 자동차의 이름이 공백이거나 5자를 초과한 경우 예외처리
    validateTryCount() : 시도 횟수가 공백이거나 양수인 정수가 아닌 경우 예외처리

---
+++ src/main/java/racingcar/RacingGameValidator.java
@@ -0,0 +1,23 @@
+package racingcar;
+
+public class RacingGameValidator {
+
+    private static final String REGEXP_TRY_COUNT = "^[1-9][0-9]*$";
+
+    public static void validateRacingCarName(String input) {
+        if (input.isBlank()) {
+            throw new IllegalArgumentException("자동차 이름을 다시 입력해주세요.");
+        }
+        if (input.length() > 5) {
+            throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
+        }
+    }
+
+    public static void validateTryCount(String input) {
+        if (input.isBlank()) {
+            throw new IllegalArgumentException("시도 횟수는 공백일 수 없습니다.");
+        } else if (!input.matches(REGEXP_TRY_COUNT)) {
+            throw new IllegalArgumentException("시도 횟수는 양수인 정수만 입력될 수 있습니다.");
+        }
+    }
+}
