03ddfd3a9014b1438cf292ae8e919ac08117666a
pywoo <kfdsy0103@naver.com>
2025-10-25 19:52:02 +0900

    feat: 사용자 입출력 처리 기능 구현
    inputRacingCarNames() : 경주할 자동차 이름을 입력받아 String으로 반환합니다.
    inputTryCount() : 시도할 횟수를 질문하여 String으로 반환합니다.
    printRacingStartMessage() : 실행 결과를 보여줄 때, 시작 메시지를 출력합니다.
    printRacingStatus() : 차량들을 인자로 받아 현재 게임 상태를 나타냅니다.
    printRacingWinnerNames() : 승리한 차량 이름 목록을 인자로 받아 최종 우승자를 출력합니다.

---
+++ src/main/java/racingcar/ConsoleIO.java
@@ -0,0 +1,33 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+
+public class ConsoleIO {
+
+    public static String inputRacingCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        return Console.readLine();
+    }
+
+    public static String inputTryCount() {
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        return Console.readLine();
+    }
+
+    public static void printRacingStartMessage() {
+        System.out.println("실행 결과");
+    }
+
+    public static void printRacingStatus(List<RacingCar> racingCarList) {
+        racingCarList.stream()
+                .map(racingCar -> racingCar.getName() + " : " + "-".repeat(racingCar.getForwardCount()))
+                .forEach(System.out::println);
+        System.out.println();
+    }
+
+    public static void printRacingWinnerNames(List<String> winnerList) {
+        String winnerListFormat = String.join(", ", winnerList);
+        System.out.println("최종 우승자 : " + winnerListFormat);
+    }
+}
