8c1562be411f75b8ea9538e632e66e41e08bed33
pywoo <kfdsy0103@naver.com>
2025-10-25 19:22:05 +0900

    feat: 입력값이 null인 경우 예외처리 추가
    validate 단계에서 입력값이 null인 경우 생기는 예외 케이스를 추가하였습니다.

--- src/main/java/racingcar/RacingGameValidator.java
+++ src/main/java/racingcar/RacingGameValidator.java
@@ -5,7 +5,7 @@ public class RacingGameValidator {
     private static final String REGEXP_TRY_COUNT = "^[1-9][0-9]*$";
 
     public static void validateRacingCarName(String input) {
-        if (input.isBlank()) {
+        if (input == null || input.isBlank()) {
             throw new IllegalArgumentException("자동차 이름을 다시 입력해주세요.");
         }
         else if (input.length() > 5) {
@@ -17,8 +17,8 @@ public class RacingGameValidator {
     }
 
     public static void validateTryCount(String input) {
-        if (input.isBlank()) {
-            throw new IllegalArgumentException("시도 횟수는 공백일 수 없습니다.");
+        if (input == null || input.isBlank()) {
+            throw new IllegalArgumentException("시도 횟수를 다시 입력해주세요.");
         } else if (!input.matches(REGEXP_TRY_COUNT)) {
             throw new IllegalArgumentException("시도 횟수는 양수인 정수만 입력될 수 있습니다.");
         }
