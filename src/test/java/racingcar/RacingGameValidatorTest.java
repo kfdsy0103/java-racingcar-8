5cba7ac2f3e2fcece185f6df89b1727d3ffc4d56
pywoo <kfdsy0103@naver.com>
2025-10-25 19:34:25 +0900

    test: validateTryCount() 테스트 코드 작성
    1. 시도 횟수에 문자가 포함되면 실패
    2. 시도 횟수가 음수이면 실패
    3. 시도 횟수가 0이면 실패
    4. 시도 횟수가 소수인 경우 실패
    5. 시도 횟수가 양수인 정수로 주어지면 성공

--- src/test/java/racingcar/RacingGameValidatorTest.java
+++ src/test/java/racingcar/RacingGameValidatorTest.java
@@ -18,6 +18,7 @@ public class RacingGameValidatorTest {
         void 이름_쉼표_실패() {
             // given
             String racingCarName = "c,ar";
+
             // when & then
             assertThatThrownBy(() -> RacingGameValidator.validateRacingCarName(racingCarName))
                     .isInstanceOf(IllegalArgumentException.class)
@@ -29,6 +30,7 @@ public class RacingGameValidatorTest {
         void 이름_비어있음_실패() {
             // given
             String racingCarName = "";
+
             // when & then
             assertThatThrownBy(() -> RacingGameValidator.validateRacingCarName(racingCarName))
                     .isInstanceOf(IllegalArgumentException.class)
@@ -40,6 +42,7 @@ public class RacingGameValidatorTest {
         void 이름_NULL_실패() {
             // given
             String racingCarName = null;
+
             // when & then
             assertThatThrownBy(() -> RacingGameValidator.validateRacingCarName(racingCarName))
                     .isInstanceOf(IllegalArgumentException.class)
@@ -51,6 +54,7 @@ public class RacingGameValidatorTest {
         void 이름_공백_실패() {
             // given
             String racingCarName = "   ";
+
             // when & then
             assertThatThrownBy(() -> RacingGameValidator.validateRacingCarName(racingCarName))
                     .isInstanceOf(IllegalArgumentException.class)
@@ -62,6 +66,7 @@ public class RacingGameValidatorTest {
         void 이름_5자초과_실패() {
             // given
             String racingCarName = "helloMyCar";
+
             // when & then
             assertThatThrownBy(() -> RacingGameValidator.validateRacingCarName(racingCarName))
                     .isInstanceOf(IllegalArgumentException.class)
@@ -73,8 +78,85 @@ public class RacingGameValidatorTest {
         void 이름_성공() {
             // given
             String racingCarName = "hello";
+
             // when & then
             assertThatCode(() -> RacingGameValidator.validateRacingCarName(racingCarName))
+                    .doesNotThrowAnyException();
+        }
+    }
+
+    @Nested
+    @DisplayName("validateTryCount()")
+    class ValidateTryCount {
+
+        @Test
+        @DisplayName("시도 횟수가 null인 경우 실패한다.")
+        void 시도횟수_NULL_실패() {
+            // given
+            String tryCount = null;
+
+            // when & then
+            assertThatThrownBy(() -> RacingGameValidator.validateTryCount(tryCount))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage("시도 횟수를 다시 입력해주세요.");
+        }
+
+        @Test
+        @DisplayName("시도 횟수에 문자가 포함된 경우 실패한다.")
+        void 시도횟수_문자_실패() {
+            // given
+            String tryCount = "10a";
+
+            // when & then
+            assertThatThrownBy(() -> RacingGameValidator.validateTryCount(tryCount))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage("시도 횟수는 양수인 정수만 입력될 수 있습니다.");
+        }
+
+        @Test
+        @DisplayName("시도 횟수가 음수인 경우 실패한다.")
+        void 시도횟수_음수_실패() {
+            // given
+            String tryCount = "-10";
+
+            // when & then
+            assertThatThrownBy(() -> RacingGameValidator.validateTryCount(tryCount))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage("시도 횟수는 양수인 정수만 입력될 수 있습니다.");
+        }
+
+        @Test
+        @DisplayName("시도 횟수가 0인 경우 실패한다.")
+        void 시도횟수_0_실패() {
+            // given
+            String tryCount = "0";
+
+            // when & then
+            assertThatThrownBy(() -> RacingGameValidator.validateTryCount(tryCount))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage("시도 횟수는 양수인 정수만 입력될 수 있습니다.");
+        }
+
+        @Test
+        @DisplayName("시도 횟수가 소수인 경우 실패한다.")
+        void 시도횟수_소수_실패() {
+            // given
+            String tryCount = "5.5";
+
+            // when & then
+            assertThatThrownBy(() -> RacingGameValidator.validateTryCount(tryCount))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage("시도 횟수는 양수인 정수만 입력될 수 있습니다.");
+        }
+
+        @Test
+        @DisplayName("시도 횟수가 양수인 정수로 주어진 경우 성공한다.")
+        void 시도횟수_양수정수_성공() {
+            // given
+            String tryCount = "10";
+
+            // when & then
+            assertThatCode(() -> RacingGameValidator.validateTryCount(tryCount))
                     .doesNotThrowAnyException();
         }
     }
